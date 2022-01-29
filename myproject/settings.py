4f4837a25fa7c2c1927941a6f39a24445ef8175b
Mateusz M <roadtosilver5@gmail.com>
2022-01-29 17:50:50 +0100

    heroku config

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['todo-list-djangoapp.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['todo-list-djangoapp.herokuapp.com/', 'localhost:8000']
 
 
 # Application definition
