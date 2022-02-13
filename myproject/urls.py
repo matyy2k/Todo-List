22eaee67629fe25c9a2005198afa2ecca470defb
matyy2k <49438936+matyy2k@users.noreply.github.com>
2022-02-13 23:47:04 +0100

    fix url

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('register/', register_page, name='register'),
     path('login/', login_page, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('api', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
